6a4f14eacc88f64f0d32f118f3e14ad89b780562
Sylwia Marek <sylwia1301@onet.pl>
2020-10-14 17:40:34 +0200

    Modyfikacja programu (enum)

--- src/agh/cs/lab1/World.java
+++ src/agh/cs/lab1/World.java
@@ -1,5 +1,7 @@
 package agh.cs.lab1;
 
+import java.util.Arrays;
+
 import static java.lang.System.out;
 // teraz zamiast System.out.print() wystarczy out.print()
 
@@ -8,28 +10,58 @@ public class World {
         // args - tablica Stringów
 
         out.println("Start");
-        run(args);
+        Direction[] dirs = convert(args);
+        run(dirs);
         out.println("Stop");
 
     }
 
-    public static void run(String[] args) {
-        for (String arg : args) {
+    public static void run(Direction[] dirs) {
+        for (Direction dir : dirs) {
 
-            switch(arg) {
-                case "f":
+            switch(dir) {
+                case FORWARD:
                     out.println("Zwierzak idzie do przodu");
                     break;
-                case "b":
+                case BACKWARD:
                     out.println("Zwierzak idzie do tyłu");
                     break;
-                case "r":
+                case RIGHT:
                     out.println("Zwierzak skręca w prawo");
                     break;
-                case "l":
+                case LEFT:
                     out.println("Zwierzak skręca w lewo");
                     break;
             }
         }
+    }
+
+    // zamiana tablicy łańcuchów znaków na tablicę wartości typu enum
+    public static Direction[] convert(String[] args) {
+
+        Direction[] dirs = new Direction[args.length];
+
+        int ctr = 0;
+        for (String arg : args){
+            switch(arg) {
+                case "f":
+                    dirs[ctr] = Direction.FORWARD;
+                    ctr++;
+                    break;
+                case "b":
+                    dirs[ctr] = Direction.BACKWARD;
+                    ctr++;
+                    break;
+                case "r":
+                    dirs[ctr] = Direction.RIGHT;
+                    ctr++;
+                    break;
+                case "l":
+                    dirs[ctr] = Direction.LEFT;
+                    ctr++;
+                    break;
+            }
+        }
+        return Arrays.copyOfRange(dirs,0,ctr);
     }
 }
